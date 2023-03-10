daa72a66774437c0cd1cd5653c559fdc396ef5ba
Richard <richardwillian166@hotmail.com>
2023-03-10 19:00:42 -0300

    Entrega de Desafio 1

---
+++ Desafio.rb
@@ -0,0 +1,8 @@
+print "Qual é o seu nome? "
+nome = gets.chomp
+print "Qual é seu sobrenome? "
+sobrenome = gets.chomp
+print "Digite sua idade? "
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo! Sua idade é de #{idade} anos."
